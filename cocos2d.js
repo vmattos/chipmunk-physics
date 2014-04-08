fe9a63ed4e404f0f6c744d03bd8bdaaf0fca3885
Vitor Mattos <vitor.mattos@caelum.com.br>
2014-04-08 16:33:37 -0300

    Setando arquivos iniciais

--- cocos2d.js
+++ cocos2d.js
@@ -39,7 +39,8 @@
         //SingleEngineFile:'',
         appFiles:[
             'src/resource.js',
-            'src/myApp.js'//add your own files in order here
+            'src/myApp.js',
+            'src/PhysicScene.js'
         ]
     };
 
