7fef48f67d8c62e4fa8a6986bb98af3d8c0372c2
Vitor Mattos <vitor.mattos@caelum.com.br>
2014-04-08 17:27:45 -0300

    Bola

--- src/PhysicScene.js
+++ src/PhysicScene.js
@@ -6,6 +6,9 @@ var PhysicScene = cc.Scene.extend({
         this.space = new cp.Space();
 
         this.space.gravity = cp.v(0, -650);
+
+        this._debugNode = cc.PhysicsDebugNode.create(this.space);
+        this.addChild(this._debugNode, 10);
        
 
     },
@@ -33,5 +36,24 @@ var GameLayer = cc.Layer.extend({
 
     init:function(){
         this._super();
+
+        // cc.PhysicsSprite cria um sprite que representará um corpo
+        var ball = cc.PhysicsSprite.create("ball.png");
+        this.addChild(ball);
+
+        var raio = 32;
+
+        // Criando o corpo físico que obedecerá as leis do espaço
+        var corpo = this.space.addBody(new cp.Body(10, cp.momentForCircle(10, 0, raio, cp.v(0,0))));
+
+        // Podemos escolher a forma do corpo, a elasticidade e a fricção dele!
+        forma = this.space.addShape(new cp.CircleShape(corpo, raio, cp.v(0,0)));
+        forma.setElasticity(0.6);
+        forma.setFriction(1);
+
+        var s = cc.Director.getInstance().getWinSize();
+        corpo.setPos(cp.v(s.width/2, s.height/2));
+
+        ball.setBody(corpo)
     },
 });
